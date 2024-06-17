8ddefa8b8c7a4ac9b5686a982d5aaeb02a3c8b6c
christiancortese18 <cortesechristian18@gmail.com>
2024-06-17 10:55:28 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace STEAM2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: Contatti
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        // GET: paginaGioco
+        public ViewResult paginaGioco(int? idGioco)
+        {
+            ViewBag.idGioco = idGioco;
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="STEAM2.MvcApplication" Language="C#" %>
